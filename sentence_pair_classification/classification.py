aa2bc99081e52862ed227b75a7d56670350c45d6
Fangping Lan <fangpinglan0116@gmail.com>
2024-01-20 04:18:47 +0000

    run llama2-7b

---
+++ sentence_pair_classification/classification.py
@@ -0,0 +1,117 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from typing import List, Optional
+import fire
+
+from llama import Llama, Dialog
+import sys
+from os.path import dirname, abspath
+root = dirname(dirname(dirname(abspath(__file__)))) # directory to CompareDBDoc
+sys.path.append(root) 
+
+from sentence_pair_classification.data import construct_dialogs_in_batch, convert_to_csv
+from tqdm import tqdm
+
+import logging
+import time
+import datetime
+current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+log_filename = "./logs/running{}.log".format(current_time)
+logging.basicConfig(format='%(asctime)s - %(module)s - %(message)s', filename=log_filename, filemode='w', level=logging.INFO)
+
+path_to_csv_file='../../CompareDBDoc/datasets/data_humanlabel/sampled_data_Jan19.csv'
+path_to_output_file = './llama2_7b_data/llama2_7b_response_data_Jan19.csv'
+begin_index = 0
+batch_size = 1
+
+# for old, new in data.construct_pairs_sequence(path_to_csv_file):
+#     print(old)
+#     print("=========="*5)
+#     print(new)
+#     exit()
+
+
+def main(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    max_seq_len: int = 512,
+    max_batch_size: int = 8,
+    max_gen_len: Optional[int] = None,
+):
+    """
+    Entry point of the program for generating text using a pretrained model.
+
+    Args:
+        ckpt_dir (str): The directory containing checkpoint files for the pretrained model.
+        tokenizer_path (str): The path to the tokenizer model used for text encoding/decoding.
+        temperature (float, optional): The temperature value for controlling randomness in generation.
+            Defaults to 0.6.
+        top_p (float, optional): The top-p sampling parameter for controlling diversity in generation.
+            Defaults to 0.9.
+        max_seq_len (int, optional): The maximum sequence length for input prompts. Defaults to 512.
+        max_batch_size (int, optional): The maximum batch size for generating sequences. Defaults to 8.
+        max_gen_len (int, optional): The maximum length of generated sequences. If None, it will be
+            set to the model's max sequence length. Defaults to None.
+    """
+    logging.info('Begin to run...')
+    generator = Llama.build(
+        ckpt_dir=ckpt_dir,
+        tokenizer_path=tokenizer_path,
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+    )
+
+    labeled_datas = []
+    exceptional_pair_ids = []
+    try:
+        for pair_ids, dialogs in tqdm(construct_dialogs_in_batch(path_to_csv_file, batch=batch_size, begin_index=begin_index), total=(129-begin_index)//5):
+            
+            try:
+                results = generator.chat_completion(
+                    dialogs,  # type: ignore
+                    max_gen_len=max_gen_len,
+                    temperature=temperature,
+                    top_p=top_p,
+                )
+            except Exception as e:
+                exceptional_pair_ids += pair_ids
+                logging.error("Exception occurred", exc_info=True)
+                logging.info('Exception pair_ids: {}'.format(pair_ids))
+                break
+
+            else:
+                for idx, result in enumerate(results):
+
+                    record = {
+                        'pair_id': pair_ids[idx],
+                        "response": result['generation']['content']
+                    }
+
+                    labeled_datas.append(record)
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
+    finally:
+        logging.info('Exceptional pair_ids: {}'.format(exceptional_pair_ids))
+        logging.info('Finish to run to label data...')
+        logging.info('Save labeled data...')
+        try:
+            filename = ".".join(path_to_output_file.split('.')[:-1])
+            extension = path_to_output_file.split('.')[-1].strip()
+            # print("filename", filename)
+            # print("extension", extension)
+            convert_to_csv(labeled_datas, filename + "_idx" + str(begin_index) + "_" + str(current_time)  + "." + extension)
+        except Exception as e:
+            logging.error("Save Exception occurred", exc_info=True)
+        else: 
+            logging.info('Successfully save labeled data!')
+
+if __name__ == "__main__":
+    fire.Fire(main)
+    # ckpt_dir = "../llama-2-7b-chat"
+    # tokenizer_path = "../tokenizer.model"
+    # max_seq_len = 4096 
+    # max_batch_size = 6
+    # main(ckpt_dir, tokenizer_path, max_seq_len=max_seq_len, max_batch_size=max_batch_size)
---
+++ sentence_pair_classification/data.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import sys
+from os.path import dirname, abspath
+root = dirname(dirname(abspath(__file__)))
+print(root)
+sys.path.append(root)
+
+from typing import List, Optional
+from llama import Llama, Dialog
+
+def construct_dialogs_in_batch(path_to_csv_file, batch = 5, begin_index = 0):
+    data = pd.read_csv(path_to_csv_file)
+    # print(data[0:5])
+    i = begin_index
+    while i < len(data):
+        dialogs: List[Dialog] = []
+        pair_ids = []
+        for j in range(batch):
+            old_snippet = data['old_snippet'][i][2:] # remove -/+ at begining
+            new_snippet = data['new_snippet'][i][2:]
+            pair_ids.append(data['pair_id'][i+j])
+            d = [
+                    {
+                        "role": "system",
+                        "content": """
+                            Classify the type of revision between the two paragraphs and the topic of the paragraphs. The paragraph will be delimited with triple quotes.
+
+                            The categories of revisions:
+
+                            1. Textual Edits
+                                1.1 Surface Changes
+                                    - Abbreviation, Spelling, Grammer, Punctuation
+                                1.3 Word and Sentence-level Edits
+                                    - Rephrase, Paraphrase
+
+                            2. Structural Edits
+                                2.1 Organizational Changes
+                                    - Refactoring, Relocation
+
+                            3. Content Development
+                                3.1 Information Updates
+                                    - Fact Updates, Clarification, DIsambiguation
+                                3.2 Information Addition
+                                3.3 Information Deletion
+                                    - Simplification, Concision
+
+                            4. Referencing and Linking
+                                4.1 Citations, References, and Linking Changes 
+                                    - Table/Figure Reference Changes,  Linkings Changes
+
+                            5. Others
+
+                            Lables are [1.1, 1.3, 2.1, 3.1, 3.2, 3.3, 4.1, 5]. Output JSON data that includes multiple matching category indexes into an array and the topic index. The format of the JSON is {'categories':['1.3', '3.1']}.
+                            """,
+                    },
+                    {   "role": "user", 
+                        "content": """
+                            \"\"\"{}\"\"\"\n\n\"\"\"{}\"\"\"
+                            """.format(old_snippet, new_snippet)
+                    }
+            ]
+            dialogs.append(d)
+
+            
+        yield pair_ids, dialogs
+
+def convert_to_csv(data_list, csv_filename='output.csv'):
+    # Convert the list of maps to a DataFrame
+    df = pd.DataFrame(data_list)
+
+    # Write the DataFrame to a CSV file
+    df.to_csv(csv_filename, index=False)
+    print(f"CSV file '{csv_filename}' created successfully.")
+    
+if __name__ == '__main__':
+    print("llll")
+    path_to_csv_file='../../CompareDBDoc/datasets/data_humanlabel/sampled_data_Jan19.csv'
+    for pair_ids, dialogs in construct_dialogs_in_batch(path_to_csv_file):
+        print("pair_ids", pair_ids)
+        print("dialogs", dialogs)
+        exit()
+
+    # path_to_csv_file="../datasets/data_humanlabel/sampled_data_Jan19.csv"
+    # construct_pairs_with_id_without_prompt(path_to_csv_file, begin_index=0)
